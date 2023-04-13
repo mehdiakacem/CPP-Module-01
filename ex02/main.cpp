559645ca9b127a877335ed31c3547a42f90ea620
mehdiakacem <mehdiakacem20@gmail.com>
2023-04-13 02:01:01 +0000

    ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 01:39:41 by makacem           #+#    #+#             */
-/*   Updated: 2023/04/13 01:41:43 by makacem          ###   ########.fr       */
+/*   Updated: 2023/04/13 02:00:07 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 int main()
 {
-    std::string str = "HI THIS IS BRAIN";
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
 
-    
+    std::cout << "• The memory address of the string variable: " << &string << std::endl;
+    std::cout << "• The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "• The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "• The value of the string variable: " << string << std::endl;
+    std::cout << "• The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "• The value pointed to by stringREF: " << stringREF << std::endl;
 }
